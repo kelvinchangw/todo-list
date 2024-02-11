255bee6a0bf845a6cf88e469979595c876d397a4
Weissey <kelvinchangw@gmail.com>
2024-02-11 14:52:02 -0800

    Add Plus Jakarta Plus font

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// Imports
+import "./style.css";
+
+// Users can "create projects"
+// Users can "create tasks"
+// Projects have todo-items/tasks
+// Dyanmically generate todo-items/tasks
+    // Todo-items will have following properties:
+        // Title
+        // Description
+        // Due date
+        // Priority
+        // OPTIONAL: Notes
+        // OPTIONAL: Checklist
+
+
+
+// UI
+    // View all projects
+    // View all tasks in each project
+        // Expand a single task to see/edit its details
+        // Delete a todo
+
+// Store data in localStorage
+    // Function to "save tasks to localStorage" everytime a new project/tasks is created
+    // Function to "fetch tasks from localStorage" when the app is first loaded
+        // Avoid "crashing" if requested data from localStorage doesn't exist
+        // NOTE: localStorage uses JSON to send and store data
+            // NOTE: Funtions CANNOT be stored in JSON
+                // Figure out how to add methods back to the fetched object properties
+
